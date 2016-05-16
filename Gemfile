a0562f2b0a979e2b384a256bfb65aa28f170ecad
Mark Beattie <beattie.mark@gmail.com>
2016-05-17 09:41:59 +1000

    Bundler Gemfile to take it up to Rails 2.3.18 and Ruby 2.2.5

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source "https://rubygems.org"
+
+gem "rails", "~> 2.3.18"
+gem "bluecloth", "~> 2.2.0"
+gem "iconv", "~> 1.0.4"
+gem "money", "~> 5.0.0"
+gem "rake", "~> 0.9.2"
+gem "redcarpet", "~> 2.1.0"
+gem "RedCloth", "~> 4.2.8", name: "redcloth"
+gem "rspec-rails", "~> 1.3.4"
+gem "sqlite3"
+gem "test-unit", "~> 3.1.8", require: false
+gem "yahoofinance", "~> 1.2.2"
--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.18' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -38,8 +38,6 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
-  config.gem "RedCloth"
-  config.gem "yahoofinance"
 end
 
 ExceptionNotification::Notifier.exception_recipients = 'info@tradecalc.com'
